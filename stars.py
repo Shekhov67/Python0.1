b70246fb23799cde9bb33ed2627a5e76dce56715
Andy <shehs6767@mail.ru>
2023-09-10 14:06:46 +0300

    1

--- stars.py
+++ stars.py
@@ -6,7 +6,7 @@ for z in range(150):
             time.sleep(0.150)
             print(x * i)
 
-    for i in reversed([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]):
+    for i in reversed([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]):
             v = '*'
             time.sleep(0.150)
             print(v * i)
