13473fda591429c6b049d35f4e92e8dd2d7f9f4a
Andy <shehs6767@mail.ru>
2023-09-11 21:41:05 +0300

    cls

--- userInfo.py
+++ userInfo.py
@@ -1,32 +1,34 @@
 class User():
     '''Information of user'''
 
-    def __init__(self, name, surname, age, dateOfBirth):
+    def __init__(self, nameUser, surname):
         '''Конструктор'''
-        self.name = name
+        self.nameUser = nameUser
         self.surname = surname
-        self.age = age
-        self.dateOfBirth = dateOfBirth
 
     def infoUser(self):
         '''Метод'''
-        print('Name: ' + self.name + '\n' + 'Surname: ' + self.surname + '\n' + 'Age: ' + str(
-            self.age) + '\n' + 'Birthday: ' + self.dateOfBirth)
+        print('Name: ' + self.nameUser + '\n' + 'Surname: ' + self.surname)
 
 
-User1 = User('Andy', 'Jonson', 23, '12.05.2000')
-
+User1 = User('Andy', 'Jonson')
+User2 = User('Bobi', 'Ivanov')
 User1.infoUser()
+User2.infoUser()
 
 
-class langUser(User):
-    '''Language of user(потомок класса User)'''
-    def __init__(self, lang, name, age, dateOfBirth):
-        super().__init__(self, name, age, dateOfBirth)
+'''class LangUser(User):
+    Language of user(потомок класса User)
+    def __init__(self, lang, nameUser):
+        super().__init__(self, nameUser)
         self.lang = lang
 
 
-User2 = langUser("Russ", "Bobi", 26, '21.05.1999')
+User2 = LangUser("Russ", "Bobi")
 
 print(User2.lang)
+print(User2.nameUser)
+#print(User2.surname)'''
+
+
 
