b51ea45d8ca8abd1d288ce763cb54f7c9b5bef85
Andy <shehs6767@mail.ru>
2023-09-18 22:21:42 +0300

    img

--- youTube.py
+++ youTube.py
@@ -7,3 +7,16 @@ while True:
 
 exitonclick()'''
 
+''''Убрать фон
+установщик pip install rembg
+
+'''
+from rembg import remove
+from PIL import Image
+
+input_path = 'C:\\Users\\shehs\\OneDrive\\Desktop\\1.jpg'
+output_path = 'C:\\Users\\shehs\\OneDrive\\Desktop\\output_3.png'
+
+input = Image.open(input_path)
+output = remove(input)
+output.save(output_path)
